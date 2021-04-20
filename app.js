fad38a7b214cd575f671f297600c71043e1cdc5f
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-04-20 15:47:09 -0400

    basic functionality achived

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const container = document.getElementById("container")
 for (i = 1; i < 257; i++) {
   let cell = document.createElement('div');
-  cell.setAttribute("class", "cell");
+  cell.classList.add("hidden", "cell");
   cell.setAttribute("id", "cell" + i);
+  cell.addEventListener("mouseover", function () {
+   this.classList.remove("hidden") 
+  })
   container.appendChild(cell);
 }
+document.getElementById("reset").addEventListener("click", () => {
+  let cells = document.querySelectorAll(".cell");
+  cells.forEach((c) => c.classList.add("hidden"));
+})
